62fe80b40eb1e0612e30528b9e10d2b89cfb377a
tadeuasarro <tadeutas@gmail.com>
2022-02-12 20:00:57 -0300

    Project kickoff

---
+++ db/migrate/20220212225831_create_citizens.rb
@@ -0,0 +1,16 @@
+class CreateCitizens < ActiveRecord::Migration[7.0]
+  def change
+    create_table :citizens do |t|
+      t.string :name
+      t.bigint :cpf
+      t.bigint :cns
+      t.string :email
+      t.timestamp :birth_date
+      t.bigint :telefone
+      t.string :picture
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
